955271cf99b8b947756660128b1b603a1d994296
Stellamaris Nwafor <wendumaris@gmail.com>
2024-10-18 20:19:03 +0100

    updates

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -33,7 +33,7 @@ const HeroSection: React.FC = () => {
 							</Button>
 						</div> */}
 						<div className="w-full md:w-[392px] border border-[#2A2A2A] rounded-[30px] h-[59px] flex justify-between items-center gap-2 px-2 md:px-3 overflow-hidden">
-							<input type="text" placeholder="Your Email" className="bg-transparent focus:outline-none focus:ring-0 flex-grow" />
+							<input type="text" placeholder="Your Email" className="bg-transparent focus:outline-none focus:ring-0 w-[50%]" />
 							<Button className="flex gap-3 items-center !w-[40%]">
 								<span className="text-base font-medium text-white">Start Here</span>
 								<Image src={arrowRightIcon} width={16} height={16} alt="arrow right icon" />
